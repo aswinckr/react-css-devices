bbe3a603cc1ec332c8ef7a794368a166f53c815f
Aswin <aswin@Aswins-MacBook-Pro.local>
2017-07-18 13:51:34 +0530

    Initial commit | basic component

--- src/app/app.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import {render} from 'react-dom';
-import Main from './Main'; // Our custom react component
-
-// Render the main app react component into the app div.
-// For more details see: https://facebook.github.io/react/docs/top-level-api.html#react.render
-// render(<Main />, document.getElementById('app'));
-
-export default Main
